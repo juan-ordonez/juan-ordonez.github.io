3f601f2eced56660eb18939297e144e20c0c812f
Juan Ordonez <juan@gopikolo.com>
2018-02-14 13:56:13 -0800

    Fixed scrolling and fixed to bottom

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,29 @@
 const products = [{ "title": "Yipp: The Social Bookmarking App", 
 					"description" : "Yipp let's you create and curate colletions of web pages with others", 
-					"banner" : "https://i.imgur.com/3ZmAQu3.png" },
+					"banner" : "https://i.imgur.com/3ZmAQu3.png",
+					"url" : "https://www.joinyipp.com/" },
 					{ "title": "LikeWallet: The Influencer Marketing Platform", 
 					"description" : "LikeWallet provides ROI analytics for social media Influencers", 
-					"banner" : "https://i.imgur.com/gLwoSRW.png" }];
+					"banner" : "https://i.imgur.com/gLwoSRW.png", 
+					"url" : "https://www.youtube.com/watch?v=EeCHOdUHZEk"}];
+
 
 const projects = [{ "title": "Berkeley Hyperloop", 
 					"description" : "Led team of 60+ undergraduate engineers to build a functional Hyperloop pod and compete in the SpaceX Hyperloop Competition against the world’s top 27 pods", 
-					"banner" : "https://i.imgur.com/SlVDztE.jpg" },
+					"banner" : "https://i.imgur.com/SlVDztE.jpg",
+					"url" : "http://www.berkeley-hyperloop.com/" },
 					{ "title": "Drinks4Snaps: Word Of Mouth Advertising For Small Businesses", 
 					"description" : "Implemented a rewards system for promoting local restaurants through personal Snapchat accounts, increasing weekend revenue by $1,000", 
-					"banner" : "https://media.giphy.com/media/xUOwGdy9OQOQYimnfi/giphy.gif" },
+					"banner" : "https://media.giphy.com/media/xUOwGdy9OQOQYimnfi/giphy.gif",
+					"url" : "javascript:;" },
 					{ "title": "12-Bar Tensegrity Robot For NASA Missions", 
 					"description" : "Designed, prototyped, controlled and tested the world's first actuated 12-bar Tensegrity robot for planetary exploration", 
-					"banner" : "https://media.giphy.com/media/3ohs4yLSGMYG06pKG4/giphy.gif" },
+					"banner" : "https://media.giphy.com/media/3ohs4yLSGMYG06pKG4/giphy.gif",
+					"url" : "javascript:;" },
 					{ "title": "Nourish Technology: Robots Making Awesome Food.", 
 					"description" : "Designed and prototyped solutions to automatically package food using intelligent robotic systems", 
-					"banner" : "https://i.imgur.com/hYUvHfh.jpg"}];
+					"banner" : "https://i.imgur.com/hYUvHfh.jpg",
+					"url" : "https://www.youtube.com/watch?v=ttMuK_rMTpk"}];
 
 
 class Card extends React.Component {
@@ -25,10 +32,10 @@ class Card extends React.Component {
 
   		<div className="card">
 		    <div className="pageImg d-flex align-items-center">
-		      <a href="#"><img src={this.props.banner}></img></a>
+		      <a href={this.props.url} target="_blank"><img src={this.props.banner}></img></a>
 		    </div>
 		    <div className="pageInfo">
-		      <a href="#" className="pageTitle"><h1>{this.props.title}</h1></a>
+		      <a href={this.props.url} className="pageTitle" target="_blank"><h1>{this.props.title}</h1></a>
 		      <p className="pageDescription">{this.props.description}</p>
 		    </div>
 		</div>
@@ -42,11 +49,11 @@ class Cards extends React.Component {
 		var cards = [];
 		if (this.props.type == "products") {
 			for (var i = 0; i < products.length; i++ ) {
-				cards.push(<Card key={i} banner={products[i].banner} title={products[i].title} description={products[i].description} />)
+				cards.push(<Card key={i} banner={products[i].banner} title={products[i].title} description={products[i].description} url={products[i].url} />)
 			}
 		} else {
 			for (var i = 0; i < projects.length; i++ ) {
-				cards.push(<Card key={i} banner={projects[i].banner} title={projects[i].title} description={projects[i].description} />)
+				cards.push(<Card key={i} banner={projects[i].banner} title={projects[i].title} description={projects[i].description} url={projects[i].url} />)
 			}
 		}
 
@@ -144,5 +151,8 @@ $(document).on("click", "#btnAboutMe", function(){
 });
 
 function updateScroll() {
+	if ($(".container").height() > $(window).height()) {
+		$(".container").removeClass("fixed-bottom");
+	}
 	$(document).scrollTop($(".container")[0].scrollHeight);
 }
